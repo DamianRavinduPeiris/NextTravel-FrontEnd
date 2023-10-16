3a37213ac0daea50ab11e162cb02c764f17ae036
Damian Peiris. <drpeiris3@gmail.com>
2023-10-16 23:34:25 +0530

    Finalized admin login process.

--- js/adminLogin.js
+++ js/adminLogin.js
@@ -5,3 +5,38 @@ $(document).ready(function(){
 
     });
 });
+$("#loginButton").on("click",()=>{
+    if($("#username").val()=="" || $("#password").val()==""){
+        return swal("Please fill in all the fields!","OOPS!","error");
+
+    }
+    $.ajax({
+        url : "http://localhost:8080/api/v1/user/getUserByUserName?username="+$("#username").val()+"&password="+$("#password").val(),
+        method:"GET",
+        async :true,
+        headers : {
+            "Authorization" : "Bearer "+JSON.parse(localStorage.getItem("adminAuthToken"))
+
+        },
+        success : (res)=>{
+            if(res.data.authenticated){
+                return swal("Login Success!", "Redirecting you to the admin dashboard!", "success");
+
+            }
+            return swal("Bad Credentials!","OOPS!","error")
+
+        },
+        error:(error)=>{
+            return swal("An error occurred while authenticating with the server : ","","error")
+
+        }
+
+
+
+
+
+    })
+
+
+
+});
